e6a9c06a5e90665312996bc56193f9330cfeca11
Andrew Petersen <fespetersen1990@gmail.com>
2022-05-16 15:39:57 -0600

    Challenge compeleted

--- functions.test.js
+++ functions.test.js
@@ -1,14 +1,29 @@
 let myFunctions = require(`./functions`);
 
-test("test to see if 2 is equal to 2", () => {
-    expect(myFunctions.returnTwo()).toBe(2);
-});
+// test("test to see if 2 is equal to 2", () => {
+//     expect(myFunctions.returnTwo()).toBe(2);
+// });
 
-test("Testing the greeting response", () => {
-    expect(myFunctions.greeting('James')).toBe('Hello, James');
-})
+// test("Testing the greeting response", () => {
+//     expect(myFunctions.greeting('James')).toBe('Hello, James');
+// })
 
-test('Testing the sum function', () => {
-    expect(myFunctions.add(2, 4)).toBe(6);
-})
 
+describe("Math Functions", () => {
+    
+    test('Testing the sum function', () => {
+        expect(myFunctions.add(2, 4)).toBe(6);
+    })
+    
+    test('Testing the Multiply function', () => {
+        expect(myFunctions.multiply(3, 4)).toBe(12);
+    })
+
+    test('Testing the divide function', () => {
+        expect(myFunctions.divide(8, 2)).toBe(4);
+    })
+
+    test('Testing the subtract function', () => {
+        expect(myFunctions.subtract(10, 4)).toBe(6);
+    })
+});    
